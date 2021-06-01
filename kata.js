06c22062213ab10e371c2dafc2f0a0c2455f65a4
ghesketh76 <ghesketh76@gmail.com>
2021-06-01 15:43:42 -0600

    bool

--- kata.js
+++ kata.js
@@ -12,3 +12,13 @@ function positiveSum(arr) {
     
     return posArr.reduce((a,b) => a+b, 0) 
   }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    if (bool) {
+      return 'Yes'
+    } else {
+      return 'No'
+    }
+  }
