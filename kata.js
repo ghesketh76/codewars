0a2afce05537e3091fbfd590eb2dd5ff08319bd1
ghesketh76 <ghesketh76@gmail.com>
2021-06-22 10:45:46 -0600

    persistence bugger

--- kata.js
+++ kata.js
@@ -143,3 +143,14 @@ var uniqueInOrder=function(iterable){
     }
     return uniq
   }
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+function persistence(num) {
+    let i = 0
+    while(num.toString().length !== 1) {
+      num = num.toString().split('').reduce((a,b) => a*b)
+      i++
+    }
+    return i
+  }
