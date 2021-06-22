48546243b90d31097858068cb6c28f1b98c6f33d
ghesketh76 <ghesketh76@gmail.com>
2021-06-22 10:35:04 -0600

    uniq in order

--- kata.js
+++ kata.js
@@ -131,3 +131,15 @@ var uniqueCount=function(iterable){
     }
     return uniq
   }
+
+//  Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements
+
+var uniqueInOrder=function(iterable){
+    let uniq = []
+    for (i=0; i<iterable.length; i++) {
+      if (iterable[i] != iterable[i+1]) {
+        uniq.push(iterable[i])
+      }
+    }
+    return uniq
+  }
